2d36137157362a8614fdfe6467acba56cf2b4c57
B3lla <sjin09300@gmail.com>
2024-06-10 16:12:49 +0900

    feat: products entity 생성

---
+++ src/products/entities/product.entity.ts
@@ -0,0 +1,52 @@
+import { Category } from 'src/categories/entities/category.entity';
+import { ProductStatus } from 'src/product_status/entities/product_status.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('products')
+export class Product {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 50 })
+  name: string;
+
+  @Column({ length: 255 })
+  product_image_url: string;
+
+  @Column({ type: 'text' })
+  description: string;
+
+  @Column()
+  price: number;
+
+  @Column()
+  retail_price: number;
+
+  @Column()
+  shipping_fee: number;
+
+  @Column({ length: 20 })
+  shipping_details: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @ManyToOne(() => Category, (category) => category.product)
+  @JoinColumn({ name: 'categories_id' })
+  categories: Category;
+
+  @ManyToOne(() => ProductStatus, (productStatus) => productStatus.product)
+  @JoinColumn({ name: 'status_id' })
+  status: ProductStatus;
+}
