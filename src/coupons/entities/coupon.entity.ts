7e390eb439f4845d42c6bbe78d54c1fc40793988
B3lla <sjin09300@gmail.com>
2024-06-10 19:05:33 +0900

    feat: coupons entity 생성

---
+++ src/coupons/entities/coupon.entity.ts
@@ -0,0 +1,47 @@
+import { CouponIssuanceMode } from 'src/coupon-issuance-mode/entities/coupon-issuance-mode.entity';
+import { CouponStatus } from 'src/coupon-status/entities/coupon-status.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('coupons')
+export class Coupon {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 50 })
+  name: string;
+
+  @Column({ length: 30 })
+  coupon_number: string;
+
+  @Column()
+  discount: number;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @Column({ type: 'timestamp' })
+  expiration_date: Date;
+
+  // 사용 가능 최소 금액
+  @Column()
+  minimum_usage: number;
+
+  @ManyToOne(() => CouponStatus, (couponStatus) => couponStatus.coupons)
+  @JoinColumn({ name: 'stauts_id' })
+  status: CouponStatus;
+
+  // 발행 조건
+  @ManyToOne(
+    () => CouponIssuanceMode,
+    (couponIssuanceMode) => couponIssuanceMode.id,
+  )
+  @JoinColumn({ name: 'issuance_mode_id' })
+  mode: CouponIssuanceMode;
+}
