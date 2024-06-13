c3d4ab21839dc8111935c8d63a913fff8496c406
B3lla <sjin09300@gmail.com>
2024-06-13 15:09:04 +0900

    feat: auth guard 추가

---
+++ src/auth/auth.guard.ts
@@ -0,0 +1,23 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Observable } from 'rxjs';
+import { UserRole } from 'src/users/user-role.enum';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const roles = this.reflector.get<UserRole[]>('roles', context.getHandler());
+    const request = context.switchToHttp().getRequest();
+    console.log(roles);
+    if (!roles || roles == undefined) {
+      request.user = { role: UserRole.NON_MEMBER };
+      return true;
+    }
+    const user = request.user;
+    return roles.includes(user.role);
+  }
+}
