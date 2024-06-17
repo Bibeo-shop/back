01b8f5efe007e913d877c4cae769f41905436c21
B3lla <sjin09300@gmail.com>
2024-06-17 18:40:38 +0900

    chore: 불필요한 모듈 삭제 후 user-permissions 명칭 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,20 +3,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
-import { ProductStatusModule } from './product-status/product_status.module';
-import { OrdersModule } from './orders/orders.module';
-import { CouponsModule } from './coupons/coupons.module';
 import { UsersModule } from './users/users.module';
-import { CouponStatusModule } from './coupon-status/coupon-status.module';
-import { CouponIssuanceModeModule } from './coupon-issuance-mode/coupon-issuance-mode.module';
-import { ProductOptionsModule } from './product-options/product_options.module';
-import { OrderProductsModule } from './order-products/order-products.module';
-import { PointPolicyModule } from './point-policy/point-policy.module';
-import { PurchasesPointStatusModule } from './purchases-point-status/purchases-point-status.module';
-import { ReviewPointStatusModule } from './review-point-status/review-point-status.module';
-import { ExpirationDateStatusModule } from './expiration-date-status/expiration-date-status.module';
-import { CartsModule } from './carts/carts.module';
-import { CartStatusModule } from './cart-status/cart-status.module';
+import { UserPermissionModule } from './user-permissions/user-permission.module';
 
 @Module({
   imports: [
@@ -33,20 +21,8 @@ import { CartStatusModule } from './cart-status/cart-status.module';
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
-    ProductStatusModule,
-    OrdersModule,
-    CouponsModule,
     UsersModule,
-    CouponStatusModule,
-    CouponIssuanceModeModule,
-    ProductOptionsModule,
-    OrderProductsModule,
-    PointPolicyModule,
-    PurchasesPointStatusModule,
-    ReviewPointStatusModule,
-    ExpirationDateStatusModule,
-    CartsModule,
-    CartStatusModule,
+    UserPermissionModule,
   ],
   controllers: [AppController],
   providers: [AppService],
